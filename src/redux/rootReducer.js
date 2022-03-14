c27916c39cfc5dd4ae06c1211822e3a1590b6c2c
Oldu <dudkoleg@gmail.com>
2022-03-14 15:19:29 +0200

    remove simple redux folder

--- src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { getCoinsReducer } from "./getCoinsReducer";
-import { addCoinReducer } from "./addCoinReducer";
-
-export const rootReducer = combineReducers({
-  coins: getCoinsReducer,
-  portfolioCoins: addCoinReducer,
-});
