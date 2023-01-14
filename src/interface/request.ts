0eb1800ad7d072d10886b1040207b0cca645620d
Oleh <dudkoleg@gmail.com>
2023-01-14 18:47:08 -0500

    ts

---
+++ src/interface/request.ts
@@ -0,0 +1,1 @@
+export type RequestType<T, U> = (arg: T) => Promise<U>;
--- src/redux-toolkit/getTopCoinsSliceFetch.js
+++ src/redux-toolkit/getTopCoinsSliceFetch.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 const api = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=`;
+
 export const fetchTopCoins = createAsyncThunk(
   "DOWNLOAD_COINS",
   async (page) => {
